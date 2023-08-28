28272bc0794ba24104b0eb2f8b76a68446799564
carfiguru@hotmail.com <carfiguru@hotmail.com>
2023-08-27 23:37:31 -0500

    added node items

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 // server/index.js
-
+const path = require('path');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -12,4 +12,16 @@ app.get("/api", (req, res) => {
   
   app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
+  });
+
+  // have NODE serve the files for our built react app
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+  // handle GET requests to /api route
+  app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+  // all other GET requests not handled before will return our React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
